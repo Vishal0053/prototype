f323aec96d9183d7fd5d5eecb6075163ec70f50d
Vishal0053 <vishalhgiri@gmail.com>
2026-03-29 21:33:56 +0530

    Fix import: use langchain-text-splitters, update requirements

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import tempfile
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
 
 # Page Config
